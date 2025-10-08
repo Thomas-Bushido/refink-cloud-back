85cbe4b43f905ec3f7797b5447161f5722398eed
Thomas-Bushido <thomas.pereira59@outlook.fr>
2025-10-08 10:36:56 +0200

    updated controller for deployment

--- src/main/java/org/example/backbuyerv1/controller/BuyerController.java
+++ src/main/java/org/example/backbuyerv1/controller/BuyerController.java
@@ -14,7 +14,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin(
+        origins = {
+                "http://localhost:4200",
+                "https://refink-front.cleverapps.io" // <-- ton app Angular hébergée
+        },
+        methods = {RequestMethod.GET, RequestMethod.POST}
+)
 @RestController
 @RequestMapping("api/refink/buyer")
 public class BuyerController {
